b7b38f29ab2253e244ba57b23eb40ba8a0c0a759
hiepta <trananhhiep0608@gmail.com>
2025-06-23 23:07:09 +0700

    Project server

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import authRouter from './routes/auth.js'
+import connectToDatabase from './db/db.js'
+import departmentRouter from './routes/department.js'
+import employeeRouter from './routes/employee.js'
+import salaryRouter from './routes/salary.js'
+import dotenv from 'dotenv'
+
+dotenv.config();
+connectToDatabase()
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public/uploads'))
+app.use('/api/auth', authRouter)
+app.use('/api/department', departmentRouter)
+app.use('/api/employee', employeeRouter)
+app.use('/api/salary', salaryRouter)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is Running on port ${process.env.PORT}`)
+})
